2662dc13f22e2646832856ef0ca162a8068f8b0a
Retkatmun <retkatmun1@gmail.com>
2025-08-16 18:17:56 +0000

    first commit

---
+++ capitalize.py
@@ -0,0 +1,8 @@
+#capitalize use to make the first alphabet of a letter to be upper case 
+string = "my name is scholar"
+print(string.capitalize())
+
+#casefold make a texrt to lower case 
+
+case = "scHOLAR"
+print(case.casefold())
