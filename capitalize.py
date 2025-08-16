00c1744489041f03f048b88d71b6e133086e93e0
Retkatmun <retkatmun1@gmail.com>
2025-08-16 18:30:04 +0000

    count

--- capitalize.py
+++ capitalize.py
@@ -1,8 +1,15 @@
-#capitalize use to make the first alphabet of a letter to be upper case 
+#1.capitalize use to make the first alphabet of a letter to be upper case 
 string = "my name is scholar"
 print(string.capitalize())
 
-#casefold make a texrt to lower case 
-
+#2.casefold make a texrt to lower case 
 case = "scHOLAR"
 print(case.casefold())
+
+#3.center give spaces to text and allow you add anything in additon for the space
+center = "scholar"
+print(center.center(12,"$"))
+ 
+#4. count it count how many time value appear
+count = "peace"
+print(count.count("e"))
